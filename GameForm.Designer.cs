5f0973bdfbe821a45abe3edaef90a0225cb1465d
Justin Mangan <jm0015052@otc.edu>
2019-01-23 14:30:26 -0600

    commit

---
+++ GameForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HotAndCold
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBxGuess = new System.Windows.Forms.TextBox();
+            this.lblClue = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(77, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(432, 56);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "I have a number between 1 and 1000...\r\n  How fast can you guess my number?\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a guess:";
+            // 
+            // txtBxGuess
+            // 
+            this.txtBxGuess.Font = new System.Drawing.Font("Lucida Sans Unicode", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxGuess.Location = new System.Drawing.Point(246, 168);
+            this.txtBxGuess.Name = "txtBxGuess";
+            this.txtBxGuess.Size = new System.Drawing.Size(80, 49);
+            this.txtBxGuess.TabIndex = 2;
+            this.txtBxGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBxGuess.TextChanged += new System.EventHandler(this.txtBxGuess_TextChanged);
+            // 
+            // lblClue
+            // 
+            this.lblClue.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClue.Location = new System.Drawing.Point(2, 272);
+            this.lblClue.Name = "lblClue";
+            this.lblClue.Size = new System.Drawing.Size(561, 20);
+            this.lblClue.TabIndex = 3;
+            this.lblClue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(221, 347);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(139, 37);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(360, 182);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 20);
+            this.lblTimer.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(565, 396);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblClue);
+            this.Controls.Add(this.txtBxGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GameForm";
+            this.Text = "Hot and Cold";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.BackColorChanged += new System.EventHandler(this.GameForm_BackColorChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBxGuess;
+        private System.Windows.Forms.Label lblClue;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GameForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+/// <summary> /////////////////////////////////////////////////////
+/// 
+///   Project: Hot and Cold
+///   Task: Create a number guessing game where user's numeric input 
+///         changes form BackColor depending on whether user's guess 
+///         is higher or lower than random number between 0 and 1000
+///   Dev: Justin Mangan
+///   Date: 28 April 2018
+/// 
+/// </summary> \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+namespace HotAndCold
+{
+
+    /// <summary>
+    /// Partial class GameForm:Form defines GroupForm model with 
+    /// field values and Random/Stopwatch/Timer class objects
+    /// </summary>
+    public partial class GameForm : Form
+    {
+        Random randomNum = new Random();
+        Stopwatch timer = new Stopwatch();
+        TimeSpan ts;
+        private string result1;
+        private string result2;
+        private string result3;
+        private string ticked;
+        private int random;
+        private int min = 0;
+        private int max = 1000;
+
+        /// <summary>
+        /// Constructs GameForm and initializes Form
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Generate new random int, set controls on Form load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.PaleGoldenrod;
+            textBox1.BackColor = BackColor;
+            random = randomNum.Next(min, max);
+            timer.Start();
+            txtBxGuess.Focus();
+            btnReset.Enabled = false;
+        }
+
+        /// <summary>
+        /// Performs logic on user's numeric input each time it is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtBxGuess_TextChanged(object sender, EventArgs e)
+        {
+            //textBox1.BackColor = BackColor;
+            bool isNull = txtBxGuess.Text == "";
+            int guess;
+
+            if (!isNull)
+            {
+                if(int.TryParse(txtBxGuess.Text, out guess) == false)
+                {
+                    lblClue.ForeColor = Color.Red;
+                    lblClue.Text = "Must be a number";
+                }
+                else {
+                    if (guess < min + 1 || guess > max - 1)
+                    {
+                        BackColor = Color.PaleGoldenrod;                       
+                        lblClue.ForeColor = Color.Red;
+                        lblClue.Text = guess < min + 1 ? "Must be greater than " + min :
+                            "Must be less than " + max;
+                    }
+                    else
+                    {
+                        if (guess < random)
+                        {
+                            BackColor = Color.CadetBlue;
+                            lblClue.ForeColor = Color.Blue;
+                            lblClue.Text = "Too Low";
+                        }
+                        else
+                        {
+                            if (guess > random)
+                            {
+                                BackColor = Color.IndianRed;
+                                lblClue.ForeColor = Color.DarkRed;
+                                lblClue.Text = "Too High";                           
+                            }
+                            else
+                            {
+                                if (guess == random)
+                                {
+                                    timer.Stop();
+                                    ts = timer.Elapsed;                                
+                                    result1 = String.Format("You guessed my number in {0} minutes and {1:00}.{2:00} seconds!",
+                                        ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                                    result2 = String.Format("You guessed my number in {0} minute and {1:00}.{2:00} seconds!",
+                                        ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                                    result3 = String.Format("You guessed my number in {0:00}.{1:00} seconds!",
+                                        ts.Seconds, ts.Milliseconds / 10);
+                                    BackColor = Color.ForestGreen;
+                                    lblClue.ForeColor = Color.White;
+                                    lblClue.Text = ts.Minutes > 0 ? (ts.Minutes > 1 ? result1 : result2) : result3;
+                                    txtBxGuess.ReadOnly = true;
+                                    btnReset.Enabled = true;
+                                    btnReset.BackColor = Color.Orange;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                lblClue.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Button event handler generates new random int, resets: timer, controls, field values
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            random = randomNum.Next(min, max);
+            timer.Reset();
+            timer.Start();
+            txtBxGuess.Text = "";
+            txtBxGuess.ReadOnly = false;
+            txtBxGuess.Focus();
+            btnReset.Enabled = false;
+            btnReset.BackColor = Color.LightGray;
+            BackColor = Color.PaleGoldenrod;
+            lblClue.ForeColor = Color.Black;
+        }
+
+        /// <summary>
+        /// Timer event handler sets label text to current value of timer @ea tick
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ts = timer.Elapsed;
+            ticked = String.Format("{0}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            lblTimer.Text = ticked;
+        }
+
+        /// <summary>
+        /// Event handler changes textBox1 bg color when GameForm bg changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameForm_BackColorChanged(object sender, EventArgs e)
+        {
+            textBox1.BackColor = BackColor;
+        }
+    }
+}
